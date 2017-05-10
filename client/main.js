e42e30b1e321dd2927552689c16722a07218cf7e
Igor Pavlichenko <igor.pavlichenko@comon.pt>
2017-05-10 17:26:30 +0100

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,30 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+tether = require('tether');
+global.Tether = tether;
+
+
+// Import Bootstrap js npm module.
+bootstrap = require('bootstrap');
+
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
