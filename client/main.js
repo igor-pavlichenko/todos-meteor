cf53d46e114f5a828d1cf2cb9ca2a1eb4d8d0863
Igor Pavlichenko <igor.pavlichenko@comon.pt>
2017-05-11 12:59:10 +0100

    Allow adding, checking and deleting to-dos
    Use fontawesome for icons.

--- client/main.js
+++ client/main.js
@@ -1,9 +1,10 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { Todos } from '../imports//collections.js';
+import { Todos } from '../imports/collections.js';
 
 tether = require('tether');
 global.Tether = tether;
+global.Todos = Todos;
 
 
 // Import Bootstrap js npm module.
@@ -13,21 +14,37 @@ bootstrap = require('bootstrap');
 import './main.html';
 
 Template.main.onCreated(function helloOnCreated() {
-	// counter starts at 0
-	this.counter = new ReactiveVar(0);
-	console.log('lalala');
-	console.log('todos: ', Todos);
 });
 
 Template.main.helpers({
-	counter() {
-		return Template.instance().counter.get();
-	},
+	todos() {
+		return Todos.find({}, {sort: {createdAt: -1}});
+	}
 });
 
 Template.main.events({
-	'click button'(event, instance) {
-		// increment the counter when button is clicked
-		instance.counter.set(instance.counter.get() + 1);
+	"submit .new-todo": function (event) {
+		let text = event.target.text.value;
+		Todos.insert({
+			text: text,
+			createdAt: new Date(),
+		})
+		// clear input
+		event.target.text.value = "";
+
+		// prevent submit
+		return false;
 	},
+
+	"click .toggle-checked": function () {
+		console.info("toggle-checked clicked!");
+		Todos.update(this._id, {$set: {checked: !this.checked}});
+		return false;
+	},
+
+	"click .delete-todo": function () {
+		if (confirm("Are you sure?")) {
+			Todos.remove(this._id);
+		}
+	}
 });
